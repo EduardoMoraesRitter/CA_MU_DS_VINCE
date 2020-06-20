897ef747094efa35d05c6c5de7f27be4843af89f
Eduardo Ritter <eduardo.moraes.ritter@gmail.com>
2020-06-20 19:23:07 -0300

    agora vai

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express();
 var path = require('path');
 
 app.get('/', function (req, res) {
-    res.json("ok")
-    //res.sendFile(path.join(__dirname, 'simulador.html'));
+    //res.json("ok")
+    res.sendFile(path.join(__dirname, 'simulador.html'));
 });
 
 app.listen(process.env.PORT || 5000, function () {
